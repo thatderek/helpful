e80af0d156975d040fb4a801fe298ca5966f1bee
thatderek <derekhelmick@gmail.com>
2019-05-14 17:34:08 -0400

    event_payload.json functionality added

--- lambda-dev.sh
+++ lambda-dev.sh
@@ -5,6 +5,9 @@
 # and then run your code in a clone of the lambda env.
 #
 # call this app with the first argument being the name of the file to watch.
+#
+# If you need to pass it data like you might get from a CWE or whatnot, save 
+# it to the same directory as event_payload.json
 
 while true;do
   while inotifywait -q -e modify $1 ; do echo "supp" > /dev/null; done
@@ -15,7 +18,7 @@ while true;do
     -e "AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY" \
     -v `pwd`:/var/task \
     -v ~/.aws/:/home/sbx_user1051/.aws:ro \
-    lambci/lambda:python3.7 lambda_function.lambda_handler
+    lambci/lambda:python3.7 lambda_function.lambda_handler `cat event_payload.json 2> /dev/null`
 done
 ~                                                                                                                  
 ~                                                                                                                  
